18cf318da04412b54c350feb7211b2b940cfca6b
Samcyril21 <119171114+Samcyril21@users.noreply.github.com>
2023-06-26 20:44:45 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Basic-Age-Calculator-
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-d12re3)
---
+++ index.js
@@ -0,0 +1,36 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const btnEl = document.getElementById("btn");
+const birthdayEl = document.getElementById("birthday");
+const resultEl = document.getElementById("result");
+
+function calculateAge() {
+  const birthdayValue = birthdayEl.value;
+  if (birthdayValue === "") {
+    alert("Please enter your birthday");
+  } else {
+    const age = getAge(birthdayValue);
+    resultEl.innerText = `Your age is ${age} ${age > 1 ||(age < 0 && age != -1) ? "years" : "year"} old`;
+  }
+}
+
+function getAge(birthdayValue) {
+  const currentDate = new Date();
+  const birthdayDate = new Date(birthdayValue);
+  let age = currentDate.getFullYear() - birthdayDate.getFullYear();
+  const month = currentDate.getMonth() - birthdayDate.getMonth();
+  console.log(month)
+
+  if (
+    month < 0 ||
+    (month === 0 && currentDate.getDate() < birthdayDate.getDate())
+  ) {
+    age--;
+  }
+
+  return age;
+}
+
+btnEl.addEventListener("click", calculateAge);
